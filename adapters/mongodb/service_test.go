ff322a6f62a3e67477b8e15ee67f070fc44a9d9a
mblancoa <maria.blanco.arranz@gmail.com>
2024-02-15 11:39:54 +0100

    Refactoring of adapters package to adapters/mongodb

--- adapters/mongodb/service_test.go
+++ adapters/mongodb/service_test.go
@@ -1,4 +1,4 @@
-package adapters
+package mongodb
 
 import (
 	"context"
--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	beego "github.com/beego/beego/v2/server/web"
-	"github.com/mblancoa/authentication/adapters"
+	"github.com/mblancoa/authentication/adapters/mongodb"
 	"github.com/mblancoa/authentication/api"
 	"github.com/mblancoa/authentication/core"
 )
 
 func main() {
-	adapters.SetupMongodbConfiguration()
+	mongodb.SetupMongodbConfiguration()
 	core.SetupCoreConfig()
 	api.SetupApiConfiguration()
 	beego.Run()
